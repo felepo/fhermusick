e76b95635dd592f1bb868788ee887dfce4787466
Fernando Poncio <fecrackle@gmail.com>
2018-08-02 11:55:28 -0600

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+// Application name	platzimusic
+// API key	991bb190bafb26902b4cdfc1079edaba
+// Shared secret	2ab083d6012ea45011392fb4823bafac
+// Registered to	fecrackle
+
+export default {
+    appName: 'platzimusic',
+    apiKey: '991bb190bafb26902b4cdfc1079edaba',
+    secret: '2ab083d6012ea45011392fb4823bafac',
+    registeredTo: 'fecrackle'
+}
